aa58a4c2894c24a927027c52fcf5c92745cebcef
Your Name <nam.nguyenbn1997@gmail.com>
2024-06-02 15:18:10 +0700

    change name docker compose

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a7a2c5d55b0a0431f831b1eba8ac1e8d-1883819330.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
